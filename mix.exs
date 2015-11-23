5ab083fd174d961090fa381bcc5fd8ed08e336db
Buck Doyle <b@chromatin.ca>
2015-11-22 23:21:34 -0500

    Add mix test.watch

--- mix.exs
+++ mix.exs
@@ -35,7 +35,8 @@ defmodule Cr2016site.Mixfile do
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.1"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:mix_test_watch, "~> 0.2", only: :dev}]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
