2897794c420b8bfe70315d418e95974a22708e26
Buck Doyle <b@chromatin.ca>
2015-11-22 23:13:08 -0500

    Update test

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Cr2016site.PageControllerTest do
 
   test "GET /" do
     conn = get conn(), "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Clandestine Rendezvous"
   end
 end
