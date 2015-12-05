56c97d3c28c3bee6131951dc998181b0a92ffde7
Buck Doyle <b@chromatin.ca>
2015-12-05 13:57:19 -0500

    Break up integration tests

---
+++ test/integration/admin_test.exs
@@ -0,0 +1,39 @@
+defmodule Cr2016site.Integration.Admin do
+  use Cr2016site.ConnCase
+  use Cr2016site.MailgunHelper
+
+  alias Cr2016site.Pages.Login
+  alias Cr2016site.Pages.Nav
+
+  # Import Hound helpers
+  use Hound.Helpers
+
+  # Start a Hound session
+  hound_session
+
+  test "logging in as an admin" do
+    Forge.saved_user email: "francine.pascal@example.com"
+    Forge.saved_user email: "octavia.butler@example.com", crypted_password: Comeonin.Bcrypt.hashpwsalt("Xenogenesis"), admin: true
+
+    navigate_to "/"
+
+    Nav.login_link.click
+
+    Login.fill_email "octavia.butler@example.com"
+    Login.fill_password "Xenogenesis"
+    Login.submit
+
+    Nav.users_link.click
+    assert page_source =~ "francine.pascal@example.com"
+  end
+
+  test "non-admins cannot access the user list" do
+    Forge.saved_user email: "francine.pascal@example.com"
+
+    assert Nav.users_link.absent?
+
+    navigate_to "/users"
+
+    refute page_source =~ "francine.pascal@example.com"
+  end
+end
--- test/integration/registrations_test.exs
+++ test/integration/registrations_test.exs
@@ -1,4 +1,4 @@
-defmodule Cr2016site.IntegrationTest do
+defmodule Cr2016site.Integration.Registrations do
   use Cr2016site.ConnCase
   use Cr2016site.MailgunHelper
 
@@ -62,31 +62,5 @@ defmodule Cr2016site.IntegrationTest do
     assert Nav.alert_text == "Logged out"
     assert Nav.login_link.present?
     assert Nav.register_link.present?
-  end
-
-  test "logging in as an admin" do
-    Forge.saved_user email: "francine.pascal@example.com"
-    Forge.saved_user email: "octavia.butler@example.com", crypted_password: Comeonin.Bcrypt.hashpwsalt("Xenogenesis"), admin: true
-
-    navigate_to "/"
-
-    Nav.login_link.click
-
-    Login.fill_email "octavia.butler@example.com"
-    Login.fill_password "Xenogenesis"
-    Login.submit
-
-    Nav.users_link.click
-    assert page_source =~ "francine.pascal@example.com"
-  end
-
-  test "non-admins cannot access the user list" do
-    Forge.saved_user email: "francine.pascal@example.com"
-
-    assert Nav.users_link.absent?
-
-    navigate_to "/users"
-
-    refute page_source =~ "francine.pascal@example.com"
   end
 end
