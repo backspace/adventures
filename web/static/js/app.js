8e45a381f6ea8fb7181f382b9b2c9626b76d3bed
Buck Doyle <b@chromatin.ca>
2015-12-07 21:11:44 -0500

    Restore email-adding shortcut

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -24,7 +24,7 @@ $(() => {
   $("*[data-action=add-email]").click(function() {
     const email = $(this).closest("tr").children(".email").text();
 
-    const teamEmails = $("#team-emails");
+    const teamEmails = $("#user_team_emails");
     const currentValue = teamEmails.val();
 
     if (currentValue.indexOf(email) == -1) {
