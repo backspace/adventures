1f2ed0fbfe2607cf68840f44e67279eb6e954f29
Buck Doyle <b@chromatin.ca>
2015-12-19 16:10:11 -0500

    Add page object for tests

--- tests/acceptance/destinations-test.js
+++ tests/acceptance/destinations-test.js
@@ -2,6 +2,20 @@ import Ember from 'ember';
 import { test } from 'qunit';
 import moduleForAcceptance from 'adventure-gathering/tests/helpers/module-for-acceptance';
 
+import PageObject from '../page-object';
+
+const { collection, text } = PageObject;
+
+const page = PageObject.create({
+  destinations: collection({
+    itemScope: '.destination',
+
+    item: {
+      description: text('.description')
+    }
+  })
+});
+
 moduleForAcceptance('Acceptance | destinations', {
   beforeEach() {
     const store = this.application.__container__.lookup('service:store');
@@ -20,6 +34,6 @@ test('existing destinations are listed', (assert) => {
   visit('/destinations');
 
   andThen(() => {
-    assert.equal(find('.destination .description').text(), 'Ina-Karekh');
+    assert.equal(page.destinations(1).description(), 'Ina-Karekh');
   });
 });
