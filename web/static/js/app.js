38c6443035d34059b920b6d835e9785dfd1cf429
Buck Doyle <b@chromatin.ca>
2015-12-26 22:08:22 -0500

    Allow moving highlight relative to container

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -63,22 +63,35 @@ $(() => {
     const originalBackground = $(this).css('background');
     const newColour = $(this).css('color');
 
-    const offset = $(this).offset();
+    const targetSelector = $(this).data('highlight');
 
-    $(this).wrapInner('<span></span>');
+    let targets;
+
+    if (targetSelector) {
+      targets = $(this).find(targetSelector);
+    } else {
+      $(this).wrapInner('<span class=target></span>');
+      targets = $(this).find('span.target');
+    }
 
     $(this).mousemove(function(e) {
-      const x = e.pageX - offset.left;
-      const y = e.pageY - offset.top;
+      targets.each(function() {
+        // FIXME calculating this each time is wasteful
+        const offset = $(this).offset();
+        const x = e.pageX - offset.left;
+        const y = e.pageY - offset.top;
 
-      const xy = `${x} ${y}`;
+        const xy = `${x} ${y}`;
 
-      const bgWebKit = `-webkit-gradient(radial, ${xy}, 0, ${xy}, 100, from(rgba(255,255,255,0.6)), to(rgba(255,255,255,0.0))), ${newColour}`;
+        const bgWebKit = `-webkit-gradient(radial, ${xy}, 0, ${xy}, 100, from(rgba(255,255,255,0.6)), to(rgba(255,255,255,0.0))), ${newColour}`;
 
-      $(this).children('span').css({background: bgWebKit, "-webkit-background-clip": "text", "-webkit-text-fill-color": "transparent"});
+        $(this).css({background: bgWebKit, "-webkit-background-clip": "text", "-webkit-text-fill-color": "transparent"});
+      });
     }).mouseleave(function() {
-      $(this).children('span').css({background: originalBackground, "-webkit-background-clip": "initial", "-webkit-text-fill-color": "initial"});
-      $(this).removeClass("followed");
+      targets.each(function() {
+        $(this).css({background: originalBackground, "-webkit-background-clip": "initial", "-webkit-text-fill-color": "initial"});
+        $(this).removeClass("followed");
+      });
     });
   });
 });
