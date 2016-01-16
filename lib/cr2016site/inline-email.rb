33acc9adbe457b8c9233910b88143c5567b1e5b1
Buck Doyle <b@chromatin.ca>
2016-01-16 10:35:39 -0500

    Add filter to remove broken CSS

--- lib/cr2016site/inline-email.rb
+++ lib/cr2016site/inline-email.rb
@@ -4,4 +4,4 @@ BASE_URL = ARGV[0]
 
 premailer = Premailer.new('/tmp/email.html', :warn_level => Premailer::Warnings::SAFE, :base_url => BASE_URL)
 
-puts premailer.to_inline_css
+puts premailer.to_inline_css.split("\n").reject{|line| line.include?("background-attachment") && line.include?("svg")}.join("\n")
