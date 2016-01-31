1105c4423fb0a097291b510ccf887a2933a5e279
Buck Doyle <b@chromatin.ca>
2016-01-31 00:20:43 -0500

    Move graph construction into route

--- app/components/embedded-output/component.js
+++ app/components/embedded-output/component.js
@@ -10,34 +10,6 @@ import moment from 'moment';
 export default Ember.Component.extend({
   rendering: true,
 
-  getTeamMeetings() {
-    return Ember.RSVP.hash(this.get('teams').reduce((teamToMeetings, team) => {
-      teamToMeetings[team.id] = team.get('meetings');
-      return teamToMeetings;
-    }, {})).then(teamIdToMeetingList => {
-      return Ember.RSVP.hash(Object.keys(teamIdToMeetingList).reduce((teamIdToMeetingObjects, teamId) => {
-        teamIdToMeetingObjects[teamId] = Ember.RSVP.all(teamIdToMeetingList[teamId].map(meeting => {
-          return Ember.RSVP.hash({
-            meeting: meeting,
-            destination: meeting.get('destination'),
-            teams: meeting.get('teams')
-          });
-        }));
-
-        return teamIdToMeetingObjects;
-      }, {}));
-    }).then(teamIdToMeetingObjects => {
-      return Ember.RSVP.hash(Object.keys(teamIdToMeetingObjects).reduce((teamIdToMeetingObjectsWithRegion, teamId) => {
-        teamIdToMeetingObjectsWithRegion[teamId] = Ember.RSVP.all(teamIdToMeetingObjects[teamId].map(meetingObject => {
-          meetingObject.region = meetingObject.destination.get('region');
-          return meetingObject;
-        }));
-
-        return teamIdToMeetingObjectsWithRegion;
-      }, {}));
-    });
-  },
-
   didInsertElement() {
     const doc = new PDFDocument({layout: 'landscape'});
     const stream = doc.pipe(blobStream());
@@ -45,40 +17,40 @@ export default Ember.Component.extend({
     Ember.RSVP.all([fetch('/fonts/blackout.ttf'), fetch('/fonts/Oswald-Bold.ttf'), fetch('/fonts/Oswald-Regular.ttf')]).then(responses => {
       return Ember.RSVP.all(responses.map(response => response.arrayBuffer()));
     }).then(([header, bold, regular]) => {
-      this.getTeamMeetings().then(teamToMeetings => {
-        this.get('teams').forEach(team => {
-          teamToMeetings[team.id].forEach((meetingGroup, index) => {
-            doc.font(header);
-            doc.fontSize(18);
-            const rendezvousLetter = String.fromCharCode(65 + index);
-            doc.text(`Rendezvous ${rendezvousLetter}`);
+      const teamIdToMeetingObjects = this.get('teamIdToMeetingObjects');
 
-            doc.font(regular);
-            doc.fontSize(12);
-            doc.text(team.get('name'));
+      this.get('teams').forEach(team => {
+        teamIdToMeetingObjects[team.id].forEach((meetingGroup, index) => {
+          doc.font(header);
+          doc.fontSize(18);
+          const rendezvousLetter = String.fromCharCode(65 + index);
+          doc.text(`Rendezvous ${rendezvousLetter}`);
 
-            doc.text(' ');
-            doc.text(meetingGroup.region.get('name'));
+          doc.font(regular);
+          doc.fontSize(12);
+          doc.text(team.get('name'));
 
-            doc.text(' ');
-            doc.font(bold);
-            doc.text(`@${this._getRendezvousTimeForIndex(index)} meet:`);
+          doc.text(' ');
+          doc.text(meetingGroup.region.get('name'));
 
-            doc.font(regular);
-            const otherTeams = meetingGroup.teams.rejectBy('id', team.id);
-            doc.text(otherTeams.mapBy('name'));
+          doc.text(' ');
+          doc.font(bold);
+          doc.text(`@${this._getRendezvousTimeForIndex(index)} meet:`);
 
-            doc.text(' ');
-            doc.text(meetingGroup.destination.get('description'));
+          doc.font(regular);
+          const otherTeams = meetingGroup.teams.rejectBy('id', team.id);
+          doc.text(otherTeams.mapBy('name'));
 
-            doc.text(' ');
-            doc.text(' ');
-          });
-          doc.addPage();
+          doc.text(' ');
+          doc.text(meetingGroup.destination.get('description'));
+
+          doc.text(' ');
+          doc.text(' ');
         });
-
-        doc.end();
+        doc.addPage();
       });
+
+      doc.end();
     });
 
     stream.on('finish', () => {
