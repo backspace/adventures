f01280a9611d0e20b01becfc5f7aac7ea4a3c27d
Buck Doyle <b@chromatin.ca>
2016-02-05 11:55:28 -0500

    Add sorting to region select

--- app/controllers/destination.js
+++ app/controllers/destination.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  regionSort: ['name'],
+
+  sortedRegions: Ember.computed.sort('regions', 'regionSort'),
+
   actions: {
     setRegion(regionId) {
       const region = this.get('regions').findBy('id', regionId);
