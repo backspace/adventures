dfa3adfab8c46777e2bf88323bae9dff06aca378
Buck Doyle <b@chromatin.ca>
2016-02-07 12:48:57 -0500

    Add ability to create a settings document

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findRecord('settings', 'settings');
+    return this.store.findRecord('settings', 'settings').catch(() => this.store.createRecord('settings', {id: 'settings'}));
   },
 
   actions: {
--- tests/acceptance/settings-test.js
+++ tests/acceptance/settings-test.js
@@ -6,6 +6,32 @@ import Ember from 'ember';
 import page from '../pages/settings';
 
 moduleForAcceptance('Acceptance | settings', {
+  beforeEach() {
+    this.store = this.application.__container__.lookup('service:store');
+  }
+});
+
+test('a new settings document can be created and saved', function(assert) {
+  const done = assert.async();
+
+  page.visit();
+
+  andThen(() => {
+    assert.equal(page.goalField().value(), '');
+  });
+
+  page.goalField().fill('abc');
+  page.save();
+
+  andThen(() => {
+    this.store.findRecord('settings', 'settings').then(settings => {
+      assert.equal(settings.get('goal'), 'abc');
+      done();
+    });
+  });
+});
+
+moduleForAcceptance('Acceptance | settings', {
   beforeEach(assert) {
     this.store = this.application.__container__.lookup('service:store');
     const done = assert.async();
