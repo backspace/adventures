1e7253885388d883618799afd93bbbb475cf463f
Buck Doyle <b@chromatin.ca>
2016-02-12 08:06:56 -0500

    Close registration
    This shouldn’t require a commit, obviously.

--- config/prod.exs
+++ config/prod.exs
@@ -28,7 +28,7 @@ config :cr2016site,
   mailgun_key: System.get_env("MAILGUN_KEY"),
   google_analytics: true,
   request_confirmation: true,
-  registration_closed: System.get_env("REGISTRATION_CLOSED")
+  registration_closed: true
 
 # Do not print debug messages in production
 config :logger, level: :info
