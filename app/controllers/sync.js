99dd53593f40a7dba5f4d0690c036a8bad2f975f
Buck Doyle <b@chromatin.ca>
2016-02-28 23:38:05 -0500

    Convert sync to ember-concurrency task

--- app/controllers/sync.js
+++ app/controllers/sync.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 import PouchDB from 'pouchdb';
 
+import { task } from 'ember-concurrency';
+
 import Databases from 'adventure-gathering/models/databases';
 
 import config from 'adventure-gathering/config/environment';
@@ -14,37 +16,31 @@ export default Ember.Controller.extend({
 
   version: config.APP.version,
 
-  actions: {
-    sync() {
-      this.get('databases').addObject(this.get('destination'));
+  sync: task(function * () {
+    this.get('databases').addObject(this.get('destination'));
 
-      const sourceDb = Ember.getOwner(this).lookup('adapter:application').get('db');
-      const destinationDb = new PouchDB(this.get('destination'), config.emberPouch.options);
+    const sourceDb = Ember.getOwner(this).lookup('adapter:application').get('db');
+    const destinationDb = new PouchDB(this.get('destination'), config.emberPouch.options);
 
-      const syncPromise = sourceDb.sync(destinationDb);
+    const syncPromise = sourceDb.sync(destinationDb);
 
-      this.set('isSyncing', true);
-      this.set('result', undefined);
+    this.set('result', undefined);
+    this.set('syncPromise', syncPromise);
 
-      syncPromise.then(result => {
-        Ember.run(() => {
-          if (!this.isDestroyed) {
-            this.set('result', result);
-            this.set('isSyncing', false);
-          }
-        });
-      }).catch(error => {
-        Ember.run(() => {
-          console.log('error with sync:');
-          console.log(stringify(error));
-          this.set('error', error);
-          this.set('isSyncing', false);
-        });
+    yield syncPromise.then(result => {
+      Ember.run(() => {
+        this.set('result', result);
       });
+    }).catch(error => {
+      Ember.run(() => {
+        console.log('error with sync:');
+        console.log(stringify(error));
+        this.set('error', error);
+      });
+    });
+  }),
 
-      this.set('syncPromise', syncPromise);
-    },
-
+  actions: {
     setDestination(destination) {
       this.set('destination', destination);
     }
