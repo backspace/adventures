63e7209a486bc0bcaa9344fe1a9e103aeee056c2
Buck Doyle <b@chromatin.ca>
2017-05-20 10:21:57 -0700

    Add ember-web-app
    This replaces my piecemeal implementation.

---
+++ config/manifest.js
@@ -0,0 +1,24 @@
+/*jshint node:true*/
+'use strict';
+
+module.exports = function(/* environment, appConfig */) {
+  // See https://github.com/san650/ember-web-app#documentation for a list of
+  // supported properties
+
+  return {
+    name: "adventure-gathering",
+    short_name: "adventure-gathering",
+    description: "",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
+    icons: [
+      {
+        src: '/favicon.png',
+        sizes: '300x300',
+        type: 'image/png'
+      }
+    ]
+  };
+}
