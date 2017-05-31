2c2ccf919975edee0d42a4b3e26011acba1a2726
Buck Doyle <b@chromatin.ca>
2017-05-31 15:12:07 -0700

    Add workaround for create-or-find settings error

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -2,7 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findRecord('settings', 'settings').catch(() => this.store.createRecord('settings', {id: 'settings'}));
+    const pouch = this.store.adapterFor('settings').db;
+
+    // FIXME this is a hideous workaround for https://github.com/emberjs/data/issues/2150
+    // It’ll break if the ember-pouch record ID mapping changes
+    return pouch.get('settings_2_settings').then(() => {
+      return Ember.run(() => this.store.findRecord('settings', 'settings'));
+    }).catch(() => {
+      return Ember.run(() => this.store.createRecord('settings', {id: 'settings'}));
+    });
   },
 
   actions: {
