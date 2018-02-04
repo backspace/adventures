c23b39a778345950a313c203817819e2dedc5784
Buck Doyle <b@chromatin.ca>
2018-02-03 20:19:56 -0600

    Update unit tests with codemod
    It doesn’t work on acceptance tests, sadly.

--- tests/unit/models/destination-test.js
+++ tests/unit/models/destination-test.js
@@ -1,62 +1,65 @@
-import { moduleForModel, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
 
-moduleForModel('destination', 'Unit | Model | destination', {
-  needs: ['model:meeting', 'model:region', 'service:puzzles']
-});
+import { run } from '@ember/runloop';
 
-test('it generates a suggested mask', function(assert) {
-  let model = this.subject({answer: 'ABC123'});
+module('Unit | Model | destination', function(hooks) {
+  setupTest(hooks);
 
-  assert.equal(model.get('suggestedMask'), 'ABC___');
-});
+  test('it generates a suggested mask', function(assert) {
+    let model = run(() => this.owner.lookup('service:store').createRecord('destination', {answer: 'ABC123'}));
 
-test('it suggests a maximum of three of the rightmost blanks', function(assert) {
-  const model = this.subject({answer: 'A0C1234'});
-
-  assert.equal(model.get('suggestedMask'), 'A0C1___');
-});
-
-test('a mask is valid when it matches the answer', function(assert) {
-  let model = this.subject({
-    answer: 'ABC123',
-    mask: 'ABC___'
+    assert.equal(model.get('suggestedMask'), 'ABC___');
   });
 
-  assert.ok(model.get('maskIsValid'));
-});
+  test('it suggests a maximum of three of the rightmost blanks', function(assert) {
+    const model = run(() => this.owner.lookup('service:store').createRecord('destination', {answer: 'A0C1234'}));
 
-test('a mask is valid when it masks a subset of the answer’s numbers', function(assert) {
-  let model = this.subject({
-    answer: 'ABC123',
-    mask: 'ABC1_3'
+    assert.equal(model.get('suggestedMask'), 'A0C1___');
   });
 
-  assert.ok(model.get('maskIsValid'));
-});
+  test('a mask is valid when it matches the answer', function(assert) {
+    let model = run(() => this.owner.lookup('service:store').createRecord('destination', {
+      answer: 'ABC123',
+      mask: 'ABC___'
+    }));
 
-test('a mask is invalid if it has a different length', function(assert) {
-  let model = this.subject({
-    answer: 'ABC123',
-    mask: 'AB___'
+    assert.ok(model.get('maskIsValid'));
   });
 
-  assert.notOk(model.get('maskIsValid'));
-});
+  test('a mask is valid when it masks a subset of the answer’s numbers', function(assert) {
+    let model = run(() => this.owner.lookup('service:store').createRecord('destination', {
+      answer: 'ABC123',
+      mask: 'ABC1_3'
+    }));
 
-test('a mask is invalid when a letter differs', function(assert) {
-  let model = this.subject({
-    answer: 'ABC123',
-    mask: 'ABD___'
+    assert.ok(model.get('maskIsValid'));
   });
 
-  assert.notOk(model.get('maskIsValid'));
-});
+  test('a mask is invalid if it has a different length', function(assert) {
+    let model = run(() => this.owner.lookup('service:store').createRecord('destination', {
+      answer: 'ABC123',
+      mask: 'AB___'
+    }));
 
-test('a mask is invalid when it has no blanks', function(assert) {
-  let model = this.subject({
-    answer: 'ABC123',
-    mask: 'ABC123'
+    assert.notOk(model.get('maskIsValid'));
   });
 
-  assert.notOk(model.get('maskIsValid'));
+  test('a mask is invalid when a letter differs', function(assert) {
+    let model = run(() => this.owner.lookup('service:store').createRecord('destination', {
+      answer: 'ABC123',
+      mask: 'ABD___'
+    }));
+
+    assert.notOk(model.get('maskIsValid'));
+  });
+
+  test('a mask is invalid when it has no blanks', function(assert) {
+    let model = run(() => this.owner.lookup('service:store').createRecord('destination', {
+      answer: 'ABC123',
+      mask: 'ABC123'
+    }));
+
+    assert.notOk(model.get('maskIsValid'));
+  });
 });
--- tests/unit/services/puzzles-test.js
+++ tests/unit/services/puzzles-test.js
@@ -1,117 +1,122 @@
 import EmberObject from '@ember/object';
-import { moduleFor, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
 
-moduleFor('service:puzzles', 'Unit | Service | puzzles | chooseBlankIndex');
+module('Unit | Service | puzzles | chooseBlankIndex', function(hooks) {
+  setupTest(hooks);
 
-test('it chooses a blank index', function(assert) {
-  const service = this.subject();
+  test('it chooses a blank index', function(assert) {
+    const service = this.owner.lookup('service:puzzles');
 
-  assert.equal(
-    service.chooseBlankIndex({
-      answer: '123',
-      mask: '1_3',
-      goalDigit: 9
-    }),
-  1, 'expected the only blank index');
+    assert.equal(
+      service.chooseBlankIndex({
+        answer: '123',
+        mask: '1_3',
+        goalDigit: 9
+      }),
+    1, 'expected the only blank index');
 
-  assert.equal(
-    service.chooseBlankIndex({
-      answer: '321',
-      mask: '___',
-      goalDigit: 9
-    }),
-  2, 'expected the farthest-away blank index');
+    assert.equal(
+      service.chooseBlankIndex({
+        answer: '321',
+        mask: '___',
+        goalDigit: 9
+      }),
+    2, 'expected the farthest-away blank index');
 
-  assert.equal(
-    service.chooseBlankIndex({
-      answer: '222',
-      mask: '___',
-      goalDigit: 9
-    }),
-  0, 'expected the first farthest-away blank index');
+    assert.equal(
+      service.chooseBlankIndex({
+        answer: '222',
+        mask: '___',
+        goalDigit: 9
+      }),
+    0, 'expected the first farthest-away blank index');
+  });
+
+  test('throws if the answer and mask have a mismatch', function(assert) {
+    const service = this.owner.lookup('service:puzzles');
+
+    assert.throws(() => {
+      service.chooseBlankIndex({
+        answer: '123',
+        mask: '1_5',
+        goalDigit: 9
+      });
+    }, 'expected an error when a non-masked digit did not match');
+
+    assert.throws(() => {
+      service.chooseBlankIndex({
+        answer: '123',
+        mask: '1_34',
+        goalDigit: 9
+      });
+    }, 'expected an error when the mask was longer than the answer');
+
+    assert.throws(() => {
+      service.chooseBlankIndex({
+        answer: '123',
+        mask: '123',
+        goalDigit: 9
+      });
+    }, 'expected an error when the mask had no blanks');
+  });
 });
 
-test('throws if the answer and mask have a mismatch', function(assert) {
-  const service = this.subject();
+module('Unit | Service | puzzles | teamDigitsForAnswerAndGoalDigits', function(hooks) {
+  setupTest(hooks);
 
-  assert.throws(() => {
-    service.chooseBlankIndex({
-      answer: '123',
-      mask: '1_5',
-      goalDigit: 9
-    });
-  }, 'expected an error when a non-masked digit did not match');
-
-  assert.throws(() => {
-    service.chooseBlankIndex({
-      answer: '123',
-      mask: '1_34',
-      goalDigit: 9
-    });
-  }, 'expected an error when the mask was longer than the answer');
-
-  assert.throws(() => {
-    service.chooseBlankIndex({
-      answer: '123',
-      mask: '123',
-      goalDigit: 9
-    });
-  }, 'expected an error when the mask had no blanks');
-});
-
-moduleFor('service:puzzles', 'Unit | Service | puzzles | teamDigitsForAnswerAndGoalDigits', {
-  beforeEach() {
+  hooks.beforeEach(function() {
     this.teamA = EmberObject.create({name: 'A team'});
     this.teamB = EmberObject.create({name: 'B team'});
 
     this.teams = [this.teamA, this.teamB];
     this.reversedTeams = [this.teamB, this.teamA];
-  }
-});
+  });
 
-test('assigns the entire difference to the team if there is only one', function(assert) {
-  const service = this.subject();
+  test('assigns the entire difference to the team if there is only one', function(assert) {
+    const service = this.owner.lookup('service:puzzles');
 
-  let teamToDigitMap = service.teamDigitsForAnswerAndGoalDigits({teams: [this.teamA], answerDigit: 3, goalDigit: 7});
+    let teamToDigitMap = service.teamDigitsForAnswerAndGoalDigits({teams: [this.teamA], answerDigit: 3, goalDigit: 7});
 
-  assert.equal(teamToDigitMap.get(this.teamA), 4);
+    assert.equal(teamToDigitMap.get(this.teamA), 4);
 
-  teamToDigitMap = service.teamDigitsForAnswerAndGoalDigits({teams: [this.teamA], answerDigit: 8, goalDigit: 2});
+    teamToDigitMap = service.teamDigitsForAnswerAndGoalDigits({teams: [this.teamA], answerDigit: 8, goalDigit: 2});
 
-  assert.equal(teamToDigitMap.get(this.teamA), -6);
-});
+    assert.equal(teamToDigitMap.get(this.teamA), -6);
+  });
 
-test('splits the difference between teams', function(assert) {
-  const service = this.subject();
+  test('splits the difference between teams', function(assert) {
+    const service = this.owner.lookup('service:puzzles');
 
-  let teamToDigitMap = service.teamDigitsForAnswerAndGoalDigits({teams: this.teams, answerDigit: 3, goalDigit: 7});
+    let teamToDigitMap = service.teamDigitsForAnswerAndGoalDigits({teams: this.teams, answerDigit: 3, goalDigit: 7});
 
-  assert.equal(teamToDigitMap.get(this.teamA), 2);
-  assert.equal(teamToDigitMap.get(this.teamB), 2);
+    assert.equal(teamToDigitMap.get(this.teamA), 2);
+    assert.equal(teamToDigitMap.get(this.teamB), 2);
 
-  teamToDigitMap = service.teamDigitsForAnswerAndGoalDigits({teams: this.teams, answerDigit: 3, goalDigit: 8});
+    teamToDigitMap = service.teamDigitsForAnswerAndGoalDigits({teams: this.teams, answerDigit: 3, goalDigit: 8});
 
-  assert.equal(teamToDigitMap.get(this.teamA), 3);
-  assert.equal(teamToDigitMap.get(this.teamB), 2);
+    assert.equal(teamToDigitMap.get(this.teamA), 3);
+    assert.equal(teamToDigitMap.get(this.teamB), 2);
 
-  teamToDigitMap = service.teamDigitsForAnswerAndGoalDigits({teams: this.reversedTeams, answerDigit: 3, goalDigit: 8});
+    teamToDigitMap = service.teamDigitsForAnswerAndGoalDigits({teams: this.reversedTeams, answerDigit: 3, goalDigit: 8});
 
-  assert.equal(teamToDigitMap.get(this.teamA), 3, 'expected the alphabetically-first team to get the larger portion');
-  assert.equal(teamToDigitMap.get(this.teamB), 2, 'expected the alphabetically-last team to get the smaller portion');
-});
+    assert.equal(teamToDigitMap.get(this.teamA), 3, 'expected the alphabetically-first team to get the larger portion');
+    assert.equal(teamToDigitMap.get(this.teamB), 2, 'expected the alphabetically-last team to get the smaller portion');
+  });
 
-test('throws if there are more than two teams', function(assert) {
-  const service = this.subject();
+  test('throws if there are more than two teams', function(assert) {
+    const service = this.owner.lookup('service:puzzles');
 
-  assert.throws(() => {
-    service.teamDigitsForAnswerAndGoalDigits({teams: [this.teamA, this.teamA, this.teamA]});
-  }, 'expected an error with more than one team');
-});
+    assert.throws(() => {
+      service.teamDigitsForAnswerAndGoalDigits({teams: [this.teamA, this.teamA, this.teamA]});
+    }, 'expected an error with more than one team');
+  });
 
-test('throws if there no teams', function(assert) {
-  const service = this.subject();
+  test('throws if there no teams', function(assert) {
+    const service = this.owner.lookup('service:puzzles');
 
-  assert.throws(() => {
-    service.teamDigitsForAnswerAndGoalDigits({teams: []});
-  }, 'expected an error with no teams');
+    assert.throws(() => {
+      service.teamDigitsForAnswerAndGoalDigits({teams: []});
+    }, 'expected an error with no teams');
+  });
 });
