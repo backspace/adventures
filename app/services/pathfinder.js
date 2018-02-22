6c1c99a4541046fcc15120c6bcaaede106fa4075
Buck Doyle <b@chromatin.ca>
2018-02-22 00:18:19 -0600

    Add preliminary pathfinder service

---
+++ app/services/pathfinder.js
@@ -0,0 +1,57 @@
+import Service from '@ember/service';
+import { computed } from '@ember/object';
+
+import jsgraphs from 'npm:js-graph-algorithms';
+
+export default Service.extend({
+  regions: computed('data', function() {
+    return Object.keys(this.get('data')).reduce((regions, key) => regions.concat(key.split('|')), []).uniq();
+  }),
+
+  hasRegion(regionName) {
+    return this.get('regions').includes(regionName);
+  },
+
+  graph: computed('data', function() {
+    const graph = new jsgraphs.WeightedDiGraph(this.get('regions.length'));
+
+    const regionToIndex = {};
+    let regionIndex = 0;
+
+    Object.entries(this.get('data')).forEach(([regions, distance]) => {
+      const [dataA, dataB] = regions.split('|');
+
+      [dataA, dataB].forEach(region => {
+        if (!regionToIndex[region]) {
+          regionToIndex[region] = regionIndex;
+          regionIndex++;
+
+          graph.node(regionToIndex[region]).label = region;
+        }
+      });
+
+      graph.addEdge(new jsgraphs.Edge(regionToIndex[dataA], regionToIndex[dataB], distance));
+      graph.addEdge(new jsgraphs.Edge(regionToIndex[dataB], regionToIndex[dataA], distance));
+    });
+
+    window.graph = graph;
+    return graph;
+  }),
+
+  distance(regionA, regionB) {
+    const graph = this.get('graph');
+
+    const regionAIndex = this.regionToIndex(regionA);
+    const regionBIndex = this.regionToIndex(regionB);
+
+    const dijkstra = new jsgraphs.Dijkstra(graph, regionAIndex);
+
+    return dijkstra.distanceTo(regionBIndex);
+  },
+
+  regionToIndex(region) {
+    const graph = this.get('graph');
+
+    return Object.keys(graph.nodeInfo).find(key => graph.nodeInfo[key].label === region);
+  }
+});
