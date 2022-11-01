291d21f036b4d6985e5f78b23665a7595d6abeb1
Buck Doyle <b@chromatin.ca>
2022-11-01 13:07:16 -0500

    Update to Phoenix 1.6

--- mix.exs
+++ mix.exs
@@ -49,12 +49,13 @@ defmodule Cr2016site.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.0"},
+      {:phoenix, "~> 1.6.0"},
       {:phoenix_pubsub, "~> 2.0"},
       {:phoenix_ecto, "~> 4.0"},
       {:ecto_sql, "~> 3.0"},
       {:postgrex, ">= 0.12.2"},
-      {:phoenix_html, "~> 2.14.3"},
+      {:phoenix_html, "~> 3.0"},
+      {:phoenix_live_view, "~> 0.16.4"},
       {:gettext, "~> 0.9"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:plug_cowboy, "~> 2.1"},
