9b0d1abd0e2a1492f4c33454d10a4ce49f962dee
Buck Doyle <b@chromatin.ca>
2023-01-16 21:21:20 -0600

    Add silencings for deprecations

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,5 +1,9 @@
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   workflow: [
+    { handler: "silence", matchId: "ember-views.curly-components.jquery-element" },
+    { handler: "silence", matchId: "computed-property.volatile" },
+    { handler: "silence", matchId: "computed-property.property" },
+    { handler: "silence", matchId: "ember-test-helpers.trigger-event.options-blob-array" }
   ]
 };
