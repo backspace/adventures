90bdfed7b7ff456f8d91f16a226054674e4d6f77
Buck Doyle <b@chromatin.ca>
2023-01-16 21:53:36 -0600

    Fix computed-property.volatile deprecation

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -2,6 +2,5 @@ self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   workflow: [
     { handler: "silence", matchId: "ember-views.curly-components.jquery-element" },
-    { handler: "silence", matchId: "computed-property.volatile" },
   ]
 };
