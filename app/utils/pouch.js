ebf6ef1acf786f73a6cec4adfb0da875894230d4
Buck Doyle <b@chromatin.ca>
2023-02-11 17:07:59 -0600

    Add Pouch debug plugin

--- app/utils/pouch.js
+++ app/utils/pouch.js
@@ -1,12 +1,14 @@
 
 import PouchDB from 'ember-pouch/pouchdb';
 import MemoryAdapter from 'pouchdb-adapter-memory';
+import PouchDBDebug from 'pouchdb-debug';
 
 window.process = window.process || {};
 window.global = window;
 
 PouchDB
   .plugin(MemoryAdapter)
+  .plugin(PouchDBDebug)
   ;
 
 export default PouchDB;
