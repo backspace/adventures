0b03020ea24a97c33ee7a1457ca5b226a5efb87a
Buck Doyle <b@chromatin.ca>
2023-02-12 18:12:59 -0600

    Update to Ember Pouch 8 beta

--- app/utils/pouch.js
+++ app/utils/pouch.js
@@ -1,14 +1,21 @@
-
-import PouchDB from 'ember-pouch/pouchdb';
-import MemoryAdapter from 'pouchdb-adapter-memory';
-import PouchDBDebug from 'pouchdb-debug';
+import PouchDB from "pouchdb-core";
+import PouchDBDebug from "pouchdb-debug";
+import PouchDBFind from "pouchdb-find";
+import PouchDBRelational from "relational-pouch";
+import indexeddb from "pouchdb-adapter-indexeddb";
+import HttpPouch from "pouchdb-adapter-http";
+import mapreduce from "pouchdb-mapreduce";
+import replication from "pouchdb-replication";
 
 window.process = window.process || {};
 window.global = window;
 
-PouchDB
-  .plugin(MemoryAdapter)
-  .plugin(PouchDBDebug)
-  ;
+PouchDB.plugin(PouchDBDebug)
+  .plugin(PouchDBFind)
+  .plugin(PouchDBRelational)
+  .plugin(indexeddb)
+  .plugin(HttpPouch)
+  .plugin(mapreduce)
+  .plugin(replication);
 
 export default PouchDB;
--- tests/test-helper.js
+++ tests/test-helper.js
@@ -2,9 +2,13 @@ import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+import PouchDB from 'adventure-gathering/utils/pouch';
+import MemoryAdapter from "pouchdb-adapter-memory";
 
 import 'ember-feature-flags/test-support/helpers/with-feature';
 
 setApplication(Application.create(config.APP));
+
+PouchDB.plugin(MemoryAdapter);
 
 start();
