2bed1848e1e6ddd2aeabf528accb2f9e2ce9bf07
Buck Doyle <b@chromatin.ca>
2023-02-18 17:37:04 -0600

    Change application route to use async/await

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,16 +1,19 @@
-import { inject as service } from '@ember/service';
-import Route from '@ember/routing/route';
+import { inject as service } from "@ember/service";
+import Route from "@ember/routing/route";
 
 export default Route.extend({
   pathfinder: service(),
   settings: service(),
   store: service(),
 
-  beforeModel() {
-    const pouch = this.get('store').adapterFor('application').db;
+  async beforeModel() {
+    const pouch = this.get("store").adapterFor("application").db;
 
-    return pouch.get('pathfinder-data').then(data => this.set('pathfinder.data', data)).catch(() => true).then(() => {
-      return this.get('settings').syncFeatures();
-    })
-  }
+    try {
+      let pathfinderData = await pouch.get("pathfinder-data");
+      this.pathfinder.set("data", pathfinderData);
+    } catch (e) {}
+
+    return this.settings.syncFeatures();
+  },
 });
