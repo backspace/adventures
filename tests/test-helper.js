1c2c21ad9efc7b77a38ee7bc7357f7ee53b7ade3
Buck Doyle <b@chromatin.ca>
2023-02-19 10:51:56 -0600

    Update Ember to 3.24

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,6 +1,8 @@
 import Application from 'adventure-gathering/app';
 import config from 'adventure-gathering/config/environment';
+import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 import PouchDB from 'adventure-gathering/utils/pouch';
 import MemoryAdapter from "pouchdb-adapter-memory";
@@ -10,5 +12,7 @@ import 'ember-feature-flags/test-support/helpers/with-feature';
 setApplication(Application.create(config.APP));
 
 PouchDB.plugin(MemoryAdapter);
+
+setup(QUnit.assert);
 
 start();
