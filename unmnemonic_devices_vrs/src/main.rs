e4c46fb97fb36782efd6672e72a692a605b9eb90
Buck Doyle <b@chromatin.ca>
2023-04-02 13:43:25 -0500

    Add output to show listening address

--- unmnemonic_devices_vrs/src/main.rs
+++ unmnemonic_devices_vrs/src/main.rs
@@ -9,7 +9,10 @@ async fn main() {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
     let db = PgPool::connect(&database_url).await.unwrap();
 
-    let listener = TcpListener::bind("127.0.0.1:3000".parse::<SocketAddr>().unwrap()).unwrap();
+    let listener_address = "127.0.0.1:3000";
+    let listener = TcpListener::bind(listener_address.parse::<SocketAddr>().unwrap()).unwrap();
+
+    println!("unmnemonic devices VRS listening on {}", listener_address);
 
     Server::from_tcp(listener)
         .expect("Failed to listen")
