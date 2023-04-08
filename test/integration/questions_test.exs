c07bbd11dd8caf07683976ade2646a7f720991b6
Buck Doyle <b@chromatin.ca>
2023-04-08 13:17:38 -0500

    Extract question form for all adventures

--- test/integration/questions_test.exs
+++ test/integration/questions_test.exs
@@ -1,7 +1,46 @@
-defmodule AdventureRegistrations.Integration.Questions do
+defmodule AdventureRegistrations.ClandestineRendezvous.Integration.Questions do
   use AdventureRegistrationsWeb.ConnCase
   use AdventureRegistrations.SwooshHelper
   use AdventureRegistrations.ClandestineRendezvous
+
+  alias AdventureRegistrations.Pages.Home
+  alias AdventureRegistrations.Pages.Nav
+
+  use Hound.Helpers
+
+  hound_session()
+
+  test "registering" do
+    navigate_to("/")
+
+    Home.fill_name("Lucy Parsons")
+    Home.fill_email("lucy@example.com")
+    Home.fill_subject("A Word to Tramps")
+
+    Home.fill_question(
+      "Can you not see that it is the industrial system and not the \"boss\" which must be changed?"
+    )
+
+    Home.submit_question()
+
+    assert Nav.info_text() == "Your question has been submitted."
+
+    [sent_email] = AdventureRegistrations.SwooshHelper.sent_email()
+    assert sent_email.to == [{"", "b@events.chromatin.ca"}]
+    assert sent_email.from == {"", "b@events.chromatin.ca"}
+
+    assert sent_email.subject ==
+             "Question from Lucy Parsons <lucy@example.com>: A Word to Tramps"
+
+    assert sent_email.text_body ==
+             "Can you not see that it is the industrial system and not the \"boss\" which must be changed?"
+  end
+end
+
+defmodule AdventureRegistrations.UnmnemonicDevices.Integration.Questions do
+  use AdventureRegistrationsWeb.ConnCase
+  use AdventureRegistrations.SwooshHelper
+  use AdventureRegistrations.UnmnemonicDevices
 
   alias AdventureRegistrations.Pages.Home
   alias AdventureRegistrations.Pages.Nav
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -66,3 +66,17 @@ defmodule AdventureRegistrations.ClandestineRendezvous do
     on_exit(fn -> Application.put_env(app, key, previous_value) end)
   end
 end
+
+defmodule AdventureRegistrations.UnmnemonicDevices do
+  use ExUnit.CaseTemplate
+
+  setup do
+    put_application_env_for_test(:adventure_registrations, :adventure, "unmnemonic-devices")
+  end
+
+  defp put_application_env_for_test(app, key, value) do
+    previous_value = Application.get_env(app, key)
+    Application.put_env(app, key, value)
+    on_exit(fn -> Application.put_env(app, key, previous_value) end)
+  end
+end
