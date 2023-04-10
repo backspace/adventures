d820556e68810eda2919364fee5364f3d8e5ddf3
Buck Doyle <b@chromatin.ca>
2023-04-10 10:39:18 -0500

    Add first use of Handlebars template
    The responses need to be application/xml for Twilio to
    work but there are no assertions on that. I’m hoping
    I can add a custom renderer.

--- unmnemonic_devices_vrs/src/lib.rs
+++ unmnemonic_devices_vrs/src/lib.rs
@@ -4,17 +4,32 @@ use axum::{
     routing::{get, post},
     Router,
 };
+use axum_template::engine::Engine;
+use handlebars::Handlebars;
 use sqlx::PgPool;
 
 use crate::routes::*;
 
+pub type AppEngine = Engine<Handlebars<'static>>;
+
 #[derive(Clone)]
 pub struct AppState {
     db: PgPool,
+    engine: AppEngine,
 }
 
 pub async fn app(db: PgPool) -> Router {
-    let shared_state = AppState { db };
+    let mut hbs = Handlebars::new();
+    hbs.register_template_string(
+        "/teams/:id",
+        "<Response><Say>Welcome, team {{name}}</Say></Response>",
+    )
+    .unwrap();
+
+    let shared_state = AppState {
+        db,
+        engine: Engine::from(hbs),
+    };
 
     Router::new()
         .route("/", get(get_root))
--- unmnemonic_devices_vrs/src/routes/teams.rs
+++ unmnemonic_devices_vrs/src/routes/teams.rs
@@ -1,22 +1,27 @@
 use axum::{
-    body::{Bytes, Full},
     extract::{Path, State},
-    response::Response,
+    response::IntoResponse,
 };
+use axum_template::{Key, Render};
+use serde::Serialize;
 
 use crate::AppState;
 
-pub async fn get_team(Path(id): Path<i32>, State(state): State<AppState>) -> Response<Full<Bytes>> {
+#[derive(Serialize)]
+pub struct Team {
+    name: String,
+}
+
+pub async fn get_team(
+    Key(key): Key,
+    Path(id): Path<i32>,
+    State(state): State<AppState>,
+) -> impl IntoResponse {
     let row: (String,) = sqlx::query_as("SELECT name FROM teams WHERE id = $1")
         .bind(id)
         .fetch_one(&state.db)
         .await
         .expect("Failed to fetch team");
 
-    let body = format!("<Response><Say>Welcome, team {}</Say></Response>", row.0);
-
-    Response::builder()
-        .header("content-type", "application/xml")
-        .body(Full::from(body))
-        .unwrap()
+    Render(key, state.engine, Team { name: row.0 })
 }
