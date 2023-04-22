9b5ea415c5cc1190afc246dbeed527707fc9b522
Buck Doyle <b@chromatin.ca>
2023-04-21 21:14:14 -0500

    Add Webpack config autofixes
    meh

--- registrations/assets/webpack.config.js
+++ registrations/assets/webpack.config.js
@@ -1,20 +1,20 @@
-const path = require('path');
-const glob = require('glob');
-const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const glob = require("glob");
+const HardSourceWebpackPlugin = require("hard-source-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = (env, options) => {
-  const devMode = options.mode !== 'production';
+  const devMode = options.mode !== "production";
 
   return {
     optimization: {
       minimizer: [
         new TerserPlugin({ cache: true, parallel: true, sourceMap: devMode }),
-        new OptimizeCSSAssetsPlugin({})
-      ]
+        new OptimizeCSSAssetsPlugin({}),
+      ],
     },
     entry: {
       "clandestine-rendezvous": glob
@@ -30,43 +30,42 @@ module.exports = (env, options) => {
         .sync("./vendor/unmnemonic-devices/**/*.js")
         .concat(["./js/unmnemonic-devices-email.js"]),
     },
-    externals: [ 'foundation-sites' ],
+    externals: ["foundation-sites"],
     output: {
-      filename: '[name].js',
-      path: path.resolve(__dirname, '../priv/static/js'),
-      publicPath: '/js/'
+      filename: "[name].js",
+      path: path.resolve(__dirname, "../priv/static/js"),
+      publicPath: "/js/",
     },
-    devtool: devMode ? 'eval-cheap-module-source-map' : undefined,
+    devtool: devMode ? "eval-cheap-module-source-map" : undefined,
     module: {
       rules: [
         {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader'
-          }
+            loader: "babel-loader",
+          },
         },
         {
           test: /\.[s]?css$/,
           use: [
             MiniCssExtractPlugin.loader,
-            'css-loader',
+            "css-loader",
             {
-              loader: 'sass-loader',
+              loader: "sass-loader",
               options: {
                 sassOptions: {
-                  includePaths: [path.resolve(__dirname, 'node_modules')]
-                }
-              }
-            }
+                  includePaths: [path.resolve(__dirname, "node_modules")],
+                },
+              },
+            },
           ],
-        }
-      ]
+        },
+      ],
     },
     plugins: [
-      new MiniCssExtractPlugin({ filename: '../css/[name].css' }),
-      new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
-    ]
-    .concat(devMode ? [new HardSourceWebpackPlugin()] : [])
-  }
+      new MiniCssExtractPlugin({ filename: "../css/[name].css" }),
+      new CopyWebpackPlugin([{ from: "static/", to: "../" }]),
+    ].concat(devMode ? [new HardSourceWebpackPlugin()] : []),
+  };
 };
