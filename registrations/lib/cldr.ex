113d8ced945a3456e612799da4bff2ed6adcd83e
Buck Doyle <b@chromatin.ca>
2023-12-17 18:50:38 -0600

    Add precompiled number format
    This silences a warning.

--- registrations/lib/cldr.ex
+++ registrations/lib/cldr.ex
@@ -2,5 +2,6 @@ defmodule AdventureRegistrations.Cldr do
   use Cldr,
     locales: [:en],
     default_locale: "en",
-    providers: [Cldr.List, Cldr.Number]
+    providers: [Cldr.List, Cldr.Number],
+    precompile_number_formats: ["#,##0"]
 end
