3c8a56c8337350d01229e7b96012cf8969eb9fc2
Buck Doyle <b@chromatin.ca>
2024-06-18 19:54:12 -0500

    Rename application

--- waydowntown/waydowntown_app/lib/main.dart
+++ waydowntown/waydowntown_app/lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Waydowntown());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Waydowntown extends StatelessWidget {
+  const Waydowntown({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Waydowntown',
       theme: ThemeData(
         // This is the theme of your application.
         //
