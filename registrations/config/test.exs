548d62bad11a3861a0ba4093d6e52ff017feaac9
Buck Doyle <b@chromatin.ca>
2024-07-02 20:15:37 -0500

    Fix deprecation
    https://hexdocs.pm/logger/1.11.4/Logger.html#warn/2

--- registrations/config/test.exs
+++ registrations/config/test.exs
@@ -7,7 +7,7 @@ config :registrations, RegistrationsWeb.Endpoint,
   server: true
 
 # Print only warnings and errors during test
-config :logger, level: :warn
+config :logger, level: :warning
 
 # Configure your database
 config :registrations, Registrations.Repo,
