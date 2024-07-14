5f649f5e3378bad3865ee5214a1158141d5220e4
Buck Doyle <b@chromatin.ca>
2024-07-14 17:34:14 -0500

    Remove unused routes

--- registrations/lib/registrations_web/router.ex
+++ registrations/lib/registrations_web/router.ex
@@ -26,30 +26,12 @@ defmodule RegistrationsWeb.Router do
     # Use the default browser stack
     pipe_through(:browser)
 
-    get("/register", RegistrationController, :new)
-    post("/register", RegistrationController, :create)
-
-    get("/login", SessionController, :new)
-    post("/login", SessionController, :create)
-    delete("/logout", SessionController, :delete)
-
     post("/teams/build", TeamController, :build)
     resources("/teams", TeamController)
 
     resources("/users", UserController, only: [:index])
     get("/details", UserController, :edit)
     put("/details", UserController, :update)
-
-    get("/account", RegistrationController, :edit)
-    put("/account", RegistrationController, :update)
-
-    get("/delete", RegistrationController, :maybe_delete)
-    put("/delete", RegistrationController, :delete)
-
-    get("/reset", ResetController, :new)
-    post("/reset", ResetController, :create)
-    get("/reset/:token", ResetController, :edit)
-    put("/reset/:token", ResetController, :update)
 
     resources("/messages", MessageController)
     post("/messages/send-backlog", MessageController, :deliver_backlog)
