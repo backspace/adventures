160c8cb0ab5cfd79b9a37c46cc65656d3712fa90
Buck Doyle <b@chromatin.ca>
2024-07-21 13:09:35 -0500

    Add autoformatting fixes

--- registrations/priv/repo/migrations/20240714173901_update_users_for_pow.exs
+++ registrations/priv/repo/migrations/20240714173901_update_users_for_pow.exs
@@ -2,10 +2,10 @@ defmodule Registrations.Repo.Migrations.CreateUsers do
   use Ecto.Migration
 
   def change do
-    rename table(:users), :crypted_password, to: :password_hash
+    rename(table(:users), :crypted_password, to: :password_hash)
 
     alter table(:users) do
-      modify :email, :string, null: false
+      modify(:email, :string, null: false)
     end
   end
 end
--- registrations/priv/repo/migrations/20240721040506_create_user_identities.exs
+++ registrations/priv/repo/migrations/20240721040506_create_user_identities.exs
@@ -3,14 +3,14 @@ defmodule Registrations.Repo.Migrations.CreateUserIdentities do
 
   def change do
     create table(:user_identities, primary_key: false) do
-      add :id, :binary_id, primary_key: true
-      add :provider, :string, null: false
-      add :uid, :string, null: false
-      add :user_id, references("users", on_delete: :nothing, type: :binary_id)
+      add(:id, :binary_id, primary_key: true)
+      add(:provider, :string, null: false)
+      add(:uid, :string, null: false)
+      add(:user_id, references("users", on_delete: :nothing, type: :binary_id))
 
       timestamps()
     end
 
-    create unique_index(:user_identities, [:uid, :provider])
+    create(unique_index(:user_identities, [:uid, :provider]))
   end
 end
