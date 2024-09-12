e191423da441e36e95ed914c1e28f5a8a27f7f22
Buck Doyle <b@chromatin.ca>
2024-09-12 18:12:12 -0500

    Remove obsolete migrations

--- registrations/priv/repo/migrations/20240907024524_change_waydowntown_incarnation_masks_to_descriptions.exs
+++
@@ -1,8 +0,0 @@
-defmodule Registrations.Repo.Migrations.ChangeWaydowntownIncarnationdescriptionsToDescriptions do
-  @moduledoc false
-  use Ecto.Migration
-
-  def change do
-    rename(table(:incarnations, prefix: "waydowntown"), :mask, to: :description)
-  end
-end
