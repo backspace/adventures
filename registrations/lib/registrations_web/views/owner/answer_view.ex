791533141f094cccb0f540e9bfaeafaf666e0459
Buck Doyle <b@chromatin.ca>
2024-09-28 17:34:56 -0500

    Remove unused alias

--- registrations/lib/registrations_web/views/owner/answer_view.ex
+++ registrations/lib/registrations_web/views/owner/answer_view.ex
@@ -1,8 +1,6 @@
 defmodule RegistrationsWeb.Owner.AnswerView do
   use JSONAPI.View, type: "answers"
 
-  alias RegistrationsWeb.Owner.AnswerView
-
   def fields do
     [:label, :order, :answer]
   end
