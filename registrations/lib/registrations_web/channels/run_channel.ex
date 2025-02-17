531c6d1c7b61e6d8d625b3f23c40eb4074a6d935
Buck Doyle <b@chromatin.ca>
2025-02-17 12:23:59 -0700

    Remove unused channel handlers and tests
    Channel messages are indeed covered by the participation
    controller tests, maybe some of this will still be needed
    in isolation, but not at the moment!

--- registrations/lib/registrations_web/channels/run_channel.ex
+++ registrations/lib/registrations_web/channels/run_channel.ex
@@ -21,21 +21,6 @@ defmodule RegistrationsWeb.RunChannel do
     end
   end
 
-  # Channels can be used in a request/response fashion
-  # by sending replies to requests from the client
-  @impl true
-  def handle_in("ping", payload, socket) do
-    {:reply, {:ok, payload}, socket}
-  end
-
-  # It is also common to receive messages from the client and
-  # broadcast to everyone in the current topic (run:lobby).
-  @impl true
-  def handle_in("shout", payload, socket) do
-    broadcast(socket, "shout", payload)
-    {:noreply, socket}
-  end
-
   # Add authorization logic here as required.
   defp authorized?(_payload) do
     true
